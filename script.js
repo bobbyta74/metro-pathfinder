64bd2e09c2e8a878d88db8b30588ead1182ab3dc
Bobby Ta <bobbyta74@gmail.com>
2023-01-30 12:38:02 +0100

    Add route and instructions output to website

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const submit = document.querySelector("#submit");
 const routedisplay = document.querySelector("#route");
 const startinput = document.querySelector("#start");
 const endinput = document.querySelector("#end");
+const routelist = document.querySelector("#routelist");
 
 
 //Array, check common
 
 let lines = [["Schneier Road", "Central", "Grand Station", "Gates Marsh"],["Torvalds Park", "Central", "Kernighan Way", "Stallman Bridge"],["Wozniak Bridge", "Grand Station", "Lecun Street", "Chollet Lane", "Poettering Wood"]];
+let linenames = ["grey", "red", "blue"]
 
 //Start and end stations, as well as the lines they are on
 let startline;
@@ -48,28 +50,49 @@ submit.addEventListener("click", function () {
         }
     }
 
-
+    let haschangeover = true;
+    let has2changeovers = false;
+    let changeover1;
 //If they're in the same line, get straightforward route
     if (startline == endline) {
         addtoroute(start, end, startline, endline);
+        haschangeover = false;
     } else {
         if (startline.indexOf("Central") > -1 && endline.indexOf("Central") > -1) {
             //Both lines have Central station
             doubleroute("Central");
+            changeover1 = "Central";
         } else if (startline.indexOf("Grand Station") > -1 && endline.indexOf("Grand Station") > -1) {
             //Both lines have Grand Station
             doubleroute("Grand Station");
+            changeover1 = "Grand Station";
         } else {
+            has2changeovers = true;
             //Lines don't share a station
             if (startline == lines[1]) {
                 addtoroute(start, "Central", startline, startline);
                 addtoroute("Grand Station", end, endline, endline);
+                changeover1 = "Central";
+                changeover2 = "Grand Station";
             } else {
                 addtoroute(start, "Grand Station", startline, startline);
                 addtoroute("Central", end, endline, endline);
+                changeover2 = "Central";
+                changeover1 = "Grand Station";
             }
         }
     }
 
-console.log(route)
+    let startlinename = linenames[lines.indexOf(startline)];
+    let endlinename = linenames[lines.indexOf(endline)];
+    
+    if (!haschangeover) {
+        routedisplay.textContent = `Take the ${startlinename} line from ${start} to ${end}`
+    } else if (!has2changeovers) {
+        routedisplay.textContent = `Take the ${startlinename} line from ${start} to ${changeover1}, then the ${endlinename} line from ${changeover1} to ${end}`
+    } else {
+        routedisplay.textContent = `Take the ${startlinename} line from ${start} to ${changeover1}, then the grey line from ${changeover1} to ${changeover2}, then the ${endlinename} line from ${changeover1} to ${end}`
+    }
+
+    routelist.textContent = "Station order: " + route.slice(0, route.length + 1).join(" -> ");
 })
